4e9bca6c9a2b2b77a9c854fc9daf222c84aa954d
soulclaws <121465112+soulclaws@users.noreply.github.com>
2025-07-20 14:32:49 +0200

    removed spaces

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# soulclaws
-
-
 import sys
-
 
 # imports functions from stats.py
 from stats import get_num_words
 from stats import char_in_text
 from stats import sort_chars_by_num
 
-
-# #### functions ####
 
 # takes a filepath to .txt file as a str and return contents as a str 
 def get_book_text(filepath):
@@ -19,19 +13,16 @@ def get_book_text(filepath):
     return text_file
 
 # #### main ####
-
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     else:
         filepath = sys.argv[1]
-
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     chars = char_in_text(text)
     sorted_chars = sort_chars_by_num(chars)
-    
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
@@ -41,14 +32,6 @@ def main():
         char = item["char"]
         num = item["num"]
         print(f"{char}: {num}")
-        
-    
     print("============= END ===============")
 
-
 main()
-
-
-
-
-
